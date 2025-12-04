30d2ab94abba6c9daa7d60068eeed53508bbdac9
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-12-04 19:18:12 +0000

    hurr

---
+++ libcert-gen.webpack.config.cjs
@@ -0,0 +1,86 @@
+const
+  _path =
+    require(
+      'path');
+const
+  _output_dir =
+    _path.resolve(
+      __dirname);
+const
+  _input_file_name =
+    `libcert-gen`;
+const
+  _input_file_path =
+    `./${_input_file_name}`;
+const
+  _output_file_name =
+    `${_input_file_name}.js`;
+const
+  _yargs_ignore =
+  { resourceRegExp:
+      /^yargs$/ };
+const
+  _yargs_helpers_ignore = {
+    resourceRegExp:
+      /^yargs\/helpers$/
+};
+const
+  _webpack =
+    require(
+     "webpack");
+const
+  _ignore_plugin =
+    _webpack.IgnorePlugin; 
+const
+  _yargs_ignore_plugin =
+    new _ignore_plugin(
+          _yargs_ignore);
+const
+  _yargs_helpers_ignore_plugin =
+    new _ignore_plugin(
+          _yargs_helpers_ignore);
+const
+  _output = {
+    path:
+      _output_dir,
+    filename:
+      _output_file_name
+};
+module.exports = {
+  entry:
+    _input_file_path,
+  output:
+    _output,
+  optimization: {
+    moduleIds: 'deterministic',
+  },
+  resolve: {
+    alias: {
+      "fs":
+        _path.resolve(
+          __dirname,
+          'node_modules/fs/fs'),
+      "path":
+        _path.resolve(
+          __dirname,
+          'node_modules/path/mod.js'),
+    },
+    fallback: {
+      "utils":
+        false,
+      "web-worker":
+        false,
+      "yargs":
+        false,
+      "yargs/helpers":
+        false,
+    }
+  },
+  externals:
+    { yargs:
+        'yargs' },
+  plugins: [
+    _yargs_ignore_plugin,
+    _yargs_helpers_ignore_plugin
+  ]
+};
