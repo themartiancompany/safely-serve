69afb8e8713a550749530b0df4c514ab91cd1b7a
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2026-02-19 14:02:23 +0100

    hurr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,35 @@
-
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-//    ----------------------------------------------------------------------
-//    Copyright © 2024, 2025
-//                Jiang Jie
-//    Copyright © 2025
-//                Pellegrino Prevete
-//
-//    All rights reserved
-//    ----------------------------------------------------------------------
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/**    ----------------------------------------------------------------------
+ *     Copyright ©
+ *       Pellegrino Prevete
+ *         2024, 2025, 2026
+ * 
+ *     All rights reserved
+ *     ----------------------------------------------------------------------
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+
+import js from "@eslint/js";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
+
+const
+  _project =
+    "safely-serve";
 
 export default defineConfig([
  { ignores: [
@@ -35,4 +41,23 @@ export default defineConfig([
          "error",
        "prefer-const":
          "error" } },
+ { files:
+     [ "**/*js,mjs,cjs}",
+       `**/${_project}*`,
+       `**/lib${_project}` ],
+   plugins:
+     { js },
+   extends:
+     [ "js/recommended" ],
+   languageOptions:
+     { globals:
+         {  ...globals.browser,
+            ...globals.node} } },
+ { files:
+     [ "**/*.js",
+       `**/${_project}*`,
+       `**/lib${_project}` ],
+   languageOptions:
+     { sourceType:
+         "commonjs" } },
 ]);
